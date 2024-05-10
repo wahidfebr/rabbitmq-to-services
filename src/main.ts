1f8385761192d0a98a94ae4cf5b9127a52f9f753
wahidfebr <wahidfebr@gmail.com>
2024-05-10 23:43:19 +0700

    feat: add sequelize and mysql driver

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const APP_PORT = 3000;
+  const APP_HOST = 'localhost';
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(APP_PORT, APP_HOST, () => {
+    console.log(`HTTP Server started at http://${APP_HOST}:${APP_PORT}`);
+  });
 }
 bootstrap();
