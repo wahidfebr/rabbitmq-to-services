393da94608198759e9ee5c4ac2304a9322337898
wahidfebr <wahidfebr@gmail.com>
2024-05-11 13:28:43 +0700

    feat: add swagger docs at baseUrl/api for task 3

--- task3/web-api/src/http/dto/http.dto.ts
+++ task3/web-api/src/http/dto/http.dto.ts
@@ -1,5 +1,18 @@
-export interface IDataDTO {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class IDataDTO {
+  @ApiProperty({
+    description: 'nama properties => string',
+    required: true,
+    default: 'wahid',
+  })
   nama: string;
+
+  @ApiProperty({
+    description: 'status properties => number',
+    required: true,
+    default: 1,
+  })
   status: number;
 }
 
