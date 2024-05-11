5331c87dc2dc780e3c5f5d8bb01125c599b98f6b
wahidfebr <wahidfebr@gmail.com>
2024-05-11 12:30:49 +0700

    feat: add package and env oonfig

--- task3/web-api/src/main.ts
+++ task3/web-api/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const APP_PORT = process.env.APP_PORT || 3000;
+  const APP_HOST = process.env.APP_HOST || '127.0.0.1';
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(APP_PORT, APP_HOST, () => {
+    console.log(`HTTP Server started at http://${APP_HOST}:${APP_PORT}`);
+  });
 }
 bootstrap();
