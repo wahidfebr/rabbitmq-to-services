31628a11b61ec05fe4abb0d7a31a84ca32471425
wahidfebr <wahidfebr@gmail.com>
2024-05-15 23:40:44 +0700

    feat(task1): add unit test on test01 service store, update, and destroy

--- task1/src/test01/service/test01.service.spec.ts
+++ task1/src/test01/service/test01.service.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Test01Service } from './test01.service';
 import { Test01 as Test01Persistence } from '../entity/test01.entity';
-import { ITest01 } from '../dto/test01.dto';
-import { Model } from 'sequelize';
+import { ITest01, ITest01StoreDTO } from '../dto/test01.dto';
+import { NotFoundException } from '@nestjs/common';
 
 describe('Test01Service', () => {
   let service: Test01Service;
@@ -81,6 +81,97 @@ describe('Test01Service', () => {
 
       expect(await service.findById(id)).toEqual(result);
       expect(Test01Persistence.findByPk).toHaveBeenCalledWith(id);
+    });
+
+    it('should throw NotFoundException if the entity is not found', async () => {
+      jest.spyOn(Test01Persistence, 'findByPk').mockResolvedValue(null);
+      await expect(service.findById(1)).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('store', () => {
+    it('should create new ITest01 then return the same ITest01 with auto increment id', async () => {
+      const newData: ITest01StoreDTO = {
+        nama: 'New Test',
+        status: 1,
+      };
+      const result: ITest01 = { ...newData, id: test01Data.at(-1).id + 1 };
+
+      jest.spyOn(Test01Persistence, 'create').mockResolvedValue({
+        ...newData,
+        id: result.id,
+        toJSON: jest.fn().mockReturnValue(result),
+      } as unknown as Test01Persistence);
+
+      expect(await service.store(newData)).toEqual(result);
+      expect(Test01Persistence.create).toHaveBeenCalledWith(newData);
+    });
+  });
+
+  describe('update', () => {
+    it('should update ITest01 then return the updated ITest01', async () => {
+      const updateData: ITest01StoreDTO = {
+        nama: 'Updated Test01',
+        status: 200,
+      };
+      const id = 1;
+      const result: ITest01 = { id: id, ...updateData };
+
+      const mockUpdateData = {
+        id: id,
+        nama: 'Test01',
+        status: 1,
+        toJSON: jest.fn().mockReturnValue(result),
+        update: jest
+          .fn()
+          .mockImplementation(
+            (newData: ITest01StoreDTO): Promise<Test01Persistence> => {
+              Object.assign(mockUpdateData, newData);
+              return Promise.resolve(mockUpdateData);
+            },
+          ),
+      } as unknown as Test01Persistence;
+
+      jest
+        .spyOn(Test01Persistence, 'findByPk')
+        .mockResolvedValue(mockUpdateData);
+
+      expect(await service.update(updateData, id)).toEqual(result);
+      expect(Test01Persistence.findByPk).toHaveBeenCalledWith(id);
+      expect(mockUpdateData.update).toHaveBeenCalledWith(updateData);
+      expect(mockUpdateData.nama).toBe('Updated Test01');
+      expect(mockUpdateData.status).toBe(200);
+      expect(mockUpdateData.toJSON).toHaveBeenCalled();
+    });
+
+    it('should throw NotFoundException if the entity is not found', async () => {
+      jest.spyOn(Test01Persistence, 'findByPk').mockResolvedValue(null);
+      await expect(
+        service.update({ nama: 'Updated Test01', status: 200 }, 1),
+      ).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('destroy', () => {
+    it('should delete ITest01 then return the deleted ITest01', async () => {
+      const index = 1;
+      const deletedData = {
+        ...mockTest01[index],
+        destroy: jest.fn(),
+      } as unknown as Test01Persistence;
+
+      jest.spyOn(Test01Persistence, 'findByPk').mockResolvedValue(deletedData);
+
+      const result: ITest01 = test01Data[index];
+      expect(await service.destroy(deletedData.id)).toEqual(result);
+      expect(Test01Persistence.findByPk).toHaveBeenCalledWith(deletedData.id);
+      expect(deletedData.destroy).toHaveBeenCalled();
+      expect(deletedData.toJSON).toHaveBeenCalled();
+    });
+
+    it('should throw NotFoundException if the entity is not found', async () => {
+      jest.spyOn(Test01Persistence, 'findByPk').mockResolvedValue(null);
+      await expect(service.destroy(1)).rejects.toThrow(NotFoundException);
     });
   });
 });
